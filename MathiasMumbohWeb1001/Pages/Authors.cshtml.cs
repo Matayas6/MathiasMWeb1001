a1b07c313392e077f635ae1c2bff4209f40e7ebe
Mathias <38738031+Matayas6@users.noreply.github.com>
2021-04-11 23:40:23 -0400

    Updated

---
+++ MathiasMumbohWeb1001/Pages/Authors.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MathiasMumbohWeb1001.Pages
+{
+    public class AuthorsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
